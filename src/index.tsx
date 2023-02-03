6d74490468d7ee46e532485554d9bd2a0e1d8c25
André Teles <andre.telestp@gmail.com>
2023-02-03 17:12:23 -0300

    chore: add mui

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css';
 
 const container = document.getElementById('root')!;
